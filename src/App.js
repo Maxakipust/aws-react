19180f642af6119b0d73c709c9cd4ea661e6738b
Max Kipust <maxakipust@gmail.com>
2020-06-05 11:04:06 -0700

    test3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test number 2
+          Test number 3
         </p>
         <a
           className="App-link"
